fff5f031b054d59cba8fb071bba78ab43f5e8f92
chagoy <angelochagoy@gmail.com>
2018-07-10 14:32:56 -0700

    deploying

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -11,7 +11,7 @@ export function LandingPage(props) {
 	return (
 		<div className="flex-row">
 			<h2>Welcome to the app</h2>
-			<p>this is the landing page. The login form should be on this page. you will have the ability to click a register button if you don't want to login.</p>
+			<p>this is the landing page. The login form should be on this page. you will have the ability to click a register button if you don't want to login. asdflkjfa</p>
 			<div className="flex-c-50">
 				<LoginForm />
 				<Link to="/register">Register</Link>
