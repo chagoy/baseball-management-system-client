b21ee2ab5c2b6965e00c747f1e4611f0719c7d77
chagoy <angelochagoy@gmail.com>
2018-11-18 09:42:09 -0800

    remove dotenv

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import {Provider} from 'react-redux';
 import App from './App';
 import store from './store';
 import registerServiceWorker from './registerServiceWorker';
-require('dotenv').config()
 
 ReactDOM.render(
 	<Provider store={store}>
